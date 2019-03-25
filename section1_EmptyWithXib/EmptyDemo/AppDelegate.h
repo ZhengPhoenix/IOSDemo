c1e13a5dafa258f856405c17eb79e2d0f0358f02
zhenghui <zhenghui@wps.cn>
2019-03-25 17:56:14 +0800

    依赖XIB的项目

---
+++ section1_EmptyWithXib/EmptyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyDemo
+//
+//  Created by 郑辉 on 2019/3/14.
+//  Copyright © 2019 郑辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
